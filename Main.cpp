83e79740d5b31c0aade33b55678bae93ef9cd5b1
ruby-rios1 <46876685+ruby-rios1@users.noreply.github.com>
2019-07-16 17:29:29 -0500

    Added functionality to Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,15 @@
 
 int main()
 {
-	return 0;
+    // Pull in the morse code text file
+    MorseCode morseCode("morse.txt");
+
+    // Tests for encode and decode for each letter of the alphabet
+    std::string encodedMessage = morseCode.encode("abcdefghijklmnopqrstuvwxyz");
+
+    //Prints out the encoded and decoded message
+    std::cout << encodedMessage << " = " << 
+        morseCode.decode(encodedMessage) << std::endl;
+
+    return 0;
 }
