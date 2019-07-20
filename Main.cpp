151c084f6e7a8dc1318f179d3c79d9dcaf16fbb5
JAWolfe04 <jawhf4@mail.umkc.edu>
2019-07-19 23:44:53 -0500

    Some little tweaks, formatting and documentation (#4)
    Shortened up some code, unified formatting and added all required documentation, assumptions and efficiencies

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,29 @@
 #include "MorseCode.h"
 #include <iostream>
 
-int main()
-{
-    // Pull in the morse code text file
+/*  Assumes morse.txt is the input file containing 26 letters of the modern 
+    English alphabet with one letter at the start of each line followed by 
+    the Morse code consisting of up to 4 '.'s and/or '_'s for that letter. 
+    There should not be any other symbols other than the letters, code, 
+    end of line characters and end of file. The encoded message must be a
+    single word of only letters of the modern English alphabet. Assumes
+    the Morse codes in the file correspond to the order of the described
+    tree in the project instructions.
+*/
+// O(n lg n)
+int main() {
+    // Pull in the Morse code text file
+    // O(1)
     MorseCode morseCode("morse.txt");
 
     // Tests for encode and decode for each letter of the alphabet
-    std::string encodedMessage = morseCode.encode("abcdefghijklmnopqrstuvwxyz");
+    // O(n lg n)
+    std::string encodedMessage = morseCode.encode("abCdefGhijklmnoPqrstUvwxYz");
 
-    //Prints out the encoded and decoded message
+    // Prints out the encoded and decoded message
+    // O(n lg n)
     std::cout << encodedMessage << " = " << 
         morseCode.decode(encodedMessage) << std::endl;
-	system("pause");
+
     return 0;
 }
--- MorseCode.h
+++ MorseCode.h
@@ -5,18 +5,64 @@
 #include <map>
 #include <string>
 
-class MorseCode
-{
+/* Builds a map and binary tree from a provided file and the description
+   of the tree in the project instructions. Encodes and decodes messages
+   following the project guidelines.
+*/
+class MorseCode {
 public:
+    /* Uses fileName to populate a map for encoding messages and creates
+       a binary tree as described in the project instructions
+
+       Throws an error if the file cannot be opened or a line in the 
+       file is not a char/code combination
+
+       O(1): while it is actually O(n) the alphabet will 
+       always be the same length so it is functionally O(1)
+    */
     MorseCode(std::string fileName);
 
+    /* Returns an encoded message using a map populated from the file
+       provided in the class constructor to convert the provided message
+       to Morse code with each letter code separated a space
+
+       Throws an error if a char other than a letter is used in the message
+
+       O(n lg n): n is the amount of letters to encode and it takes
+       O(lg n) to find the corresponding code for each letter
+    */
     std::string encode(std::string message);
+
+    /* Returns a decoded message using a tree as described in the project 
+       instructions and the provided message in Morse code with each
+       letter code separated by a space
+
+       Throws an error if an invalid code or a char other than '.' or '_'
+       is used in the message
+
+       O(n lg n): n is the amount of letters be found
+       and it takes O(lg n) to find each letter
+    */
     std::string decode(std::string message);
 
 private:
-    std::map<char, std::string> encoder;
-    Binary_Tree<char> decoder;
+    std::map<char, std::string> encoder; // Map for encoding messages
+    Binary_Tree<char> decoder; // Binary tree for decoding messages
 
+    /* Returns a char corresponding to the provided code from a
+       binary tree described in the project instructions
+
+       Throws an error if an invalid code or a char other than '.' or '_'
+       is used in the message
+
+       O(lg n): n is the amount of different types of letters
+    */
     char findLetter(std::string code);
+
+    /* Returns the root of a hard-coded binary tree as described 
+       in the project directions
+
+       O(1) --- T(17): Hard-coded binary tree
+    */
     BTNode<char>* createDecoder();
 };
